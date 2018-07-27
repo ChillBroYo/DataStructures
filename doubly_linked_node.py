cb0d31e1f66addf6b768a2c9e7786ed9ddb0cc3f
Samson Tse <sammy_tse@msn.com>
2018-07-26 23:20:51 -0700

    Added doubly linked list node setup. #3

---
+++ doubly_linked_node.py
@@ -0,0 +1,19 @@
+# Create a doubly linked list node
+
+class DoublyLinkedNode:
+    prev = None
+    next = None
+    value - None
+
+    def __init__(self, value):
+        self.value = value
+
+    def add_next(self, value):
+        pass
+
+    def add_prev(self, value)
+        pass
+
+    def remove(self):
+        pass
+
