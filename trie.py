e9897c4c9946cde2a55612c9582185c4be9075d2
Samson Tse <sammy_tse@msn.com>
2018-08-01 23:24:14 -0700

    Added trie node code and trie base

---
+++ trie.py
@@ -0,0 +1,1 @@
+# create Trie data structure
---
+++ trie_node.py
@@ -0,0 +1,62 @@
+# Trie Node: implemented to be as versitile as possible, with unique children
+
+class TrieNode:
+    def __init__(self,value=None,parent=None):
+        self.children = {}
+        self.parent = parent
+        self.value = value
+        self.times_added = 0
+    
+    # Check if a child exists within via dfs
+    def is_sub_child_dfs(self, value):
+        for node in self.children:
+            self.recursion_helper(value, None, self)
+            
+    def recursion_helper(self, value, pass_value=None, pass_node=None):
+        if self.value == value:
+            return True
+        else:
+            for node in pass_node.children:
+                return self.recursion_helper(value, None, node)
+        
+        return False
+
+
+    def add_child(self, value):
+        if self.children.get(value) == None:
+            self.children[value] = TrieNode(value, self)
+        else:
+            self.children[value].times_added += 1
+
+    def remove_child(self, value, remove_sub_children=False):
+        if self.children.get(value) == None:
+            return False
+        else:
+            return self.remove(value)
+
+            
+    def remove(self, remove_sub_children=False):
+        # Full family with keeping children
+        if self.parent and len(self.children) > 0 and remove_sub_children == False:
+            temp_val = self.children
+            parent = self.parent
+            del self.parent.children[self.value]
+            parent.children.update(temp_val)
+        # Full family with children removal
+        elif self.parent and len(self.children) > 0 and remove_sub_children == True:
+            del self.parent.children[self.value]
+        # Parent with no children (removing sub children is irrelevant here)
+        elif self.parent and len(self.children) == 0:
+            del self.parent.children[self.value]
+        # Removing root that has children with no children removal
+        elif self.parent and len(self.children) > 0 and remove_sub_children == False:
+            new_root = TrieNode(self.children[0].value)
+            del self.children[0]
+            new_root.children = self.children
+            self.children = None
+            del self
+        # Only case left should be remove all
+        else:
+            del self
+        
+        return True
