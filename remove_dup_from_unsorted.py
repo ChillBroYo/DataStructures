0b08db846817cf11004684adba3c4fc34ef51966
Samson Tse <sammy_tse@msn.com>
2018-08-01 23:23:47 -0700

    Finished initial sort. #5

--- remove_dup_from_unsorted.py
+++ remove_dup_from_unsorted.py
@@ -6,32 +6,50 @@ def dedup(unsorted_list):
     char_store = {}
     for x in unsorted_list:
         if char_store.get(x.value) == None:
-            print("unique")
             char_store[x.value] = 1
         else:
-            print("remove")
             unsorted_list.remove(x.value)
 
     return unsorted_list
 
-var = DoublyLinkedList()
-var.add(1)
-var.add(2)
-var.add(3)
-var.add(2)
-var.print_list()
-print("-----")
-var = dedup(var)
-print("-----")
-var.print_list()
+# O(1) memory usage solution
+def dedup_smaller(unsorted_list):
+    for x in range(unsorted_list.count):
+        unsorted_list[x:].remove(unsorted_list[x])
+    
+    return unsorted_list
 
-var = DoublyLinkedList()
-var.add(1)
-var.add(2)
-var.add(2)
-var.add(2)
-var.print_list()
-print("-----")
-var = dedup(var)
-print("-----")
-var.print_list()
+if __name__ == "__main__":
+    # var = DoublyLinkedList()
+    # var.add(1)
+    # var.add(2)
+    # var.add(3)
+    # var.add(2)
+    # var.print_list()
+    # print("-----")
+    # var = dedup(var)
+    # var.print_list()
+    # print("-----")
+    # print("-----")
+
+    # var = DoublyLinkedList()
+    # var.add(1)
+    # var.add(2)
+    # var.add(2)
+    # var.add(2)
+    # var.print_list()
+    # print("-----")
+    # var = dedup(var)
+    # var.print_list()
+    # print("-----")
+    # print("-----")
+
+    var = DoublyLinkedList()
+    var.add(1)
+    var.add(2)
+    var.add(2)
+    var.add(2)
+    var.print_list()
+    print("-----")
+    var = dedup_smaller(var)
+    var.print_list()
