c3a5dd3cf4ae35474913ab4de66e453ac7e3c7dc
Samson Tse <sammy_tse@msn.com>
2018-08-01 23:23:26 -0700

    Douibly linked list edits to allow improvement. #3 #5

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -6,6 +6,7 @@ class DoublyLinkedList:
         self.head = None
         self.count = 0
         self.counter_val = None
+        self.count = 0
 
     def __iter__(self):
         if self.head:
@@ -24,7 +25,30 @@ class DoublyLinkedList:
             self.counter_val = self.counter_val.next
             return temp_val
 
+    def get(self, val):
+        if self.head == None:
+            return -1
+        else:
+            count = 0
+            temp_val = self.head
+            while temp_val.next != None:
+                if temp_val.value == val:
+                    return count
+                else:
+                    temp_val = temp_val.next
+                count += 1
+    
+    def remove_duplicates(self, val):
+        if self.head == None:
+            return False
         
+        count = 0
+        has_appeared = False
+        temp_val = self.head
+        while temp_val != None or temp_val.next != None:
+            if temp_val.value == val and has_appeared:
+                pass
+                # TODO
         
     
     def add(self, value):
@@ -37,24 +61,37 @@ class DoublyLinkedList:
             
             temp_val.next = DoublyLinkedNode(value)
             temp_val.next.prev = temp_val
+        self.count += 1
+
+    def remove_at_index(self, index):
+        if index >= self.count:
+            return False
+
+        temp_val = self.head
+        for x in range(1, index):
+            temp_val = temp_val.next
+        
+        temp_val.remove()
 
     def remove(self, value, remove_all=False):
         if self.head == None:
             return False
         elif self.head.value == value and self.head.next == None:
             self.head = None
+            self.count -= 1
             return True
         elif self.head.value == value and self.head.next != None:
             self.head.next.prev = None
             self.head = self.head.next
+            self.count -= 1
             return True
         else:
             temp_val = self.head
             while temp_val.next != None:
-                print(str.format("self.tv.value = {}",temp_val.value))
-                print(str.format("self.tvn.value = {}",temp_val.next.value))
+                #print(str.format("self.tv.value = {}",temp_val.value))
+                #print(str.format("self.tvn.value = {}",temp_val.next.value))
                 if temp_val.next.value == value and temp_val.next.next != None:
-                    print(str.format("Entered here"))
+                    #print(str.format("Entered here"))
                     next = temp_val.next.next
                     temp_val.next.next.prev = temp_val
                     temp_val.next.prev = None
@@ -63,13 +100,14 @@ class DoublyLinkedList:
                     if remove_all == False:
                         break
                 elif temp_val.next.value == value and temp_val.next.next == None:
-                    print(str.format("Entered there"))
+                    #print(str.format("Entered there"))
                     temp_val.next.prev = None
                     temp_val.next = None
                     if remove_all == False:
                         break
                 else:
                     temp_val = temp_val.next
+            self.count -= 1
 
     def print_list(self):
         if self.head == None:
--- doubly_linked_node.py
+++ doubly_linked_node.py
@@ -8,3 +8,17 @@ class DoublyLinkedNode:
     
     def __iter__(self):
         return self.value
+
+    def remove(self):
+        if self.next == None and self.prev == None:
+            self.value = None
+        elif self.next == None:
+            self.value == None
+            self.prev.next = None
+            self.prev = None
+        else:
+            self.value == None
+            self.next.prev = None
+            self.prev.next = None
+            self.prev = None
+            self.next = None
