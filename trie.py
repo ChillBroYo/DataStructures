f704fdbe60ac587996730eebbeb83a5b0df730f3
Samson Tse <sammy_tse@msn.com>
2018-08-10 00:52:19 -0700

    Added more trie traversal debugging tools

--- trie.py
+++ trie.py
@@ -8,32 +8,46 @@ class Trie:
         self.root = TrieNode(None)
         self.count = 0
     
-    def dfs_traversal_print(self):
+    def dfs_traversal_print(self, path_print=False):
         if self.root == None:
             print("None")
             return False
-
-        print (self.root.value)
+        
+        if path_print == True:
+            print (str.format("{} --> ",self.root.value), end="")
+        else:
+            print(self.root.value)
         for child in self.root.children:
             # print(child)
-            self.dfs_helper(self.root.children[child])
+            self.dfs_helper(self.root.children[child], path_print)
+            if path_print == True:
+                print("")
 
     
-    def dfs_helper(self, node_to_search_through):
+    def dfs_helper(self, node_to_search_through, path_print):
         # print(parent.children[node_to_search_through])
         if len(node_to_search_through.children) == 0:
-            print (node_to_search_through.value)
+            if path_print == True:
+                print(str.format("{} <<<", node_to_search_through.value))
+            else:
+                print(node_to_search_through.value)
             return
         else:
-            print (node_to_search_through.value)
+            if path_print == True:
+                print (str(node_to_search_through.value) + " --> ", end="")
+            else:
+                print (node_to_search_through.value)
             for child in node_to_search_through.children:
-                self.dfs_helper(node_to_search_through.children[child])
+                self.dfs_helper(node_to_search_through.children[child], path_print)
 
-    def bfs_traversal(self):
+    def bfs_traversal(self, path_print=False):
         if self.root == None:
             return False
         
-        print (self.root.value)
+        if path_print == True:
+            print (str.format("{} --> ",self.root.value), end="")
+        else:
+            print(self.root.value)
         queue = deque()
         for index in self.root.children:
             queue.append(self.root.children[index])
@@ -79,7 +93,7 @@ if __name__ == "__main__":
     val.children[3].children[6].add_child(9)
     val.add_child(10)
     var.root = val
-    var.dfs_traversal_print()
+    var.dfs_traversal_print(True)
     print("-----")
     var.bfs_traversal()
     print("-----")
