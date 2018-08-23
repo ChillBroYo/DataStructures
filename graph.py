9319ef12f80f765c4f429ac47459d240d8c680a4
Samson Tse <sammy_tse@msn.com>
2018-08-23 01:18:53 -0700

    Added some changes to to graph top implement shortest path

--- graph.py
+++ graph.py
@@ -1,4 +1,6 @@
 # Create the graph structure
+import copy
+import sys
 from graph_node import GraphNode
 from graph_node_connection import GraphNodeConnection
 
@@ -76,3 +78,116 @@ class Graph():
                 break
         
         return cost
+
+    def shortest_path(self, value_to_find):
+        
+        unvisited = copy.deepcopy(self.verticies)
+        unvisited.remove(self.graph_start)
+        visited = [copy.deepcopy(self.graph_start)]
+
+        visited[0].value = 0
+        while len(unvisited) > 0:
+
+            # Grab the last item
+            work_node = visited[len(visited) - 1]
+
+            # Check all adjacent nodes and change the values only if the current is bigger
+            for edge in edges:
+                if edge.start_node == work_node and edge.end_node.value > edge.value + work_node.value:
+                    if edge.end_node not in visited:
+                        edge.end_node.value = edge.value + work_node.value
+
+            heapq.heapify(unvisited)
+            smallest_item = heapq.heappop(unvisited)
+            visited.append(smallest_item)
+
+            if smallest_item == value_to_find:
+                print(str.format("Shortest Path: {}", smallest_item.value))
+                break
+        
+        print("finish")
+            
+
+if __name__ == "__main__":
+    var = Graph()
+    A = GraphNode(sys.maxsize)
+    B = GraphNode(sys.maxsize)
+    C = GraphNode(sys.maxsize)
+    D = GraphNode(sys.maxsize)
+    E = GraphNode(sys.maxsize)
+    len_val = ["A", "B", "C", "D", "E"]
+
+    var.verticies = [A, B, C, D, E]
+    var.graph_start = A
+    var.edges.append(GraphNodeConnection(A, B, 10))
+    var.edges.append(GraphNodeConnection(B, A, 10))
+    var.edges.append(GraphNodeConnection(A, C, 5))
+    var.edges.append(GraphNodeConnection(C, A, 5))
+    var.edges.append(GraphNodeConnection(A, E, 9))
+    var.edges.append(GraphNodeConnection(E, A, 9))
+    var.edges.append(GraphNodeConnection(B, C, 1))
+    var.edges.append(GraphNodeConnection(C, B, 1))
+    var.edges.append(GraphNodeConnection(B, E, 6))
+    var.edges.append(GraphNodeConnection(E, B, 6))
+    var.edges.append(GraphNodeConnection(C, D, 46))
+    var.edges.append(GraphNodeConnection(D, C, 46))
+    var.edges.append(GraphNodeConnection(C, E, 2))
+    var.edges.append(GraphNodeConnection(E, C, 2))
+    var.edges.append(GraphNodeConnection(D, E, 25))
+    var.edges.append(GraphNodeConnection(E, D, 25))
+
+    var.shortest_path(D)
+
+
+    
+    # A = TrieNode(0)
+    # B = TrieNode(1)
+    # C = TrieNode(2)
+    # D = TrieNode(3)
+    # E = TrieNode(4)
+    # len_val = ["A", "B", "C", "D", "E"]
+
+    # edges = []
+    # unvisited_verticies = [B, C, D, E]
+    # visited_verticies = [A]
+    # visited_edge_weight = 0
+    # root = A
+    # edges.append(GraphNodeConnection(A, B, 10))
+    # edges.append(GraphNodeConnection(B, A, 10))
+    # edges.append(GraphNodeConnection(A, C, 5))
+    # edges.append(GraphNodeConnection(C, A, 5))
+    # edges.append(GraphNodeConnection(A, E, 9))
+    # edges.append(GraphNodeConnection(E, A, 9))
+    # edges.append(GraphNodeConnection(B, C, 1))
+    # edges.append(GraphNodeConnection(C, B, 1))
+    # edges.append(GraphNodeConnection(B, E, 6))
+    # edges.append(GraphNodeConnection(E, B, 6))
+    # edges.append(GraphNodeConnection(C, D, 46))
+    # edges.append(GraphNodeConnection(D, C, 46))
+    # edges.append(GraphNodeConnection(C, E, 2))
+    # edges.append(GraphNodeConnection(E, C, 2))
+    # edges.append(GraphNodeConnection(D, E, 25))
+    # edges.append(GraphNodeConnection(E, D, 25))
+    # surrounding_edges = []
+    # heapq.heapify(surrounding_edges)
+
+    # # smallest edge that connects to an unvisited vertex
+    # while len(unvisited_verticies) > 0:
+    #     work_node = visited_verticies[len(visited_verticies) - 1]
+
+    #     # get all the edges connected to current vertex and put them in priority queue
+    #     for edge in edges:
+    #         if edge.start_node == work_node:
+    #             heapq.heappush(surrounding_edges, edge)
+        
+    #     while len(surrounding_edges) > 0:
+    #         smallest_edge = heapq.heappop(surrounding_edges)
+    #         if smallest_edge.end_node not in visited_verticies:
+    #             unvisited_verticies.remove(smallest_edge.end_node)
+    #             visited_verticies.append(smallest_edge.end_node)
+    #             visited_edge_weight += smallest_edge.value
+    #             break
+    
+    # print(str.format("MST Value {}", visited_edge_weight))
+        
+
