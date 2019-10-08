97c2b1bf1f3b0bb9f5fd54a0764403831c9257bf
Samson Tse <sammy_tse@msn.com>
2019-10-08 01:26:31 -0700

    Added dll, stack and queue but without testing

--- structures.py
+++ structures.py
@@ -58,6 +58,127 @@ class DoublyLinkedList:
     def add(self, val):
         if self.head == None:
             self.head = DoublyLinkedNode(val)
+            self.tail = self.head
+            self.size += 1
+            return
+        
+        iter_node = self.head
+        while iter_node.next != None:
+            iter_node = iter_node.next
+        iter_node.next = DoublyLinkedNode(val)
+        self.tail = iter_node.next
+        self.size += 1
+    
+    def remove(self, val):
+        if self.head == None:
+            return False
+
+        if self.head.value == val and self.head.next == None:
+            self.head = None
+            self.tail = None
+        elif self.head.value == val and self.head.next != None:
+            self.head.next.prev = None
+            self.head = self.head.next
+        else:
+            iter_node = self.head
+            while iter_node.next != None:
+                if iter_node.next.value == val:
+                    if iter_node.next == self.tail:
+                        self.tail = iter_node
+                    iter_node.remove_next()
+                    self.size -= 1
+                    return True
+            return False
+        self.size -= 1
+        return True
+    
+    def remove_all(self, val):
+        while self.remove(val) == True:
+            pass
+    
+    def remove_index(self, index):
+        if index < 0 or index > self.size - 1:
+            return False
+        
+        if index == 0 and self.head.next == None:
+            self.head = None
+            self.tail = None
+        elif index == 0 and self.head.next != None:
+            self.head.next.prev = None
+            self.head = self.head.next
+        elif index == self.size - 1:
+            self.tail = self.tail.prev
+            self.tail.remove_next()
+        else:
+            count = 0
+            iter_node = self.head
+            while count != index:
+                iter_node = iter_node.next
+                count += 1
+            iter_node.prev.remove_next()
+            self.size -= 1
+        self.size -= 1
+        return True
+
+    def print(self):
+        if self.head == None:
+            print("Empty")
+        
+        iter_node = self.head
+        while iter_node != None:
+            print(iter_node.value)
+            iter_node = iter_node.next
+
+class Stack:
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.size = 0
+        self.i_list = DoublyLinkedList()
+
+    def push(self, val):
+        if self.size == self.max_size:
+            print("Stack is full")
+            return False
+
+        self.i_list.add(val)
+        self.size += 1
+        return True
+    
+    def pop(self):
+        if self.size == 0:
+            print("Stack is empty")
+            return False
+        
+        self.i_list.remove(self.i_list.head.value)
+        self.size -= 1
+        return True
+    
+    def print(self):
+        self.i_list.print()
+
+class Queue:
+    def __init__(self, max_size):
+        self.size = 0
+        self.max_size = max_size
+        self.i_list = DoublyLinkedList()
+    
+    def enqueue(self, val):
+        if self.size == self.max_size:
+            print("Queue is full")
+            return False
+        
+        self.i_list.add(val)
+        self.size += 1
+        return True
+    
+    def dequeue(self):
+        if self.size == 0:
+            print("Queue is empty")
+            return False
+        
+        self.i_list.remove_index(self.size - 1)
+        self.size -= 1
+        return True
 
 class SinglyLinkedNode:
     def __init__(self, value):
@@ -96,3 +217,27 @@ if __name__ == "__main__":
     sllist.print()
     sllist.remove_all(2)
     sllist.print()
+
+    dllist = DoublyLinkedList()
+    dllist.add(1)
+    dllist.add(2)
+    dllist.print()
+    dllist.remove(1)
+    dllist.print()
+    dllist.add(2)
+    dllist.add(2)
+    print(dllist.tail.value)
+    print(dllist.size)
+    dllist.print()
+    dllist.remove_all(2)
+    dllist.print()
+
+    stack = Stack(3)
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    stack.print()
+    stack.pop()
+    print(stack.size)
+    stack.print()
